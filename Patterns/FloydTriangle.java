ea59751f88ce97161f4008f8bc29f9dce292073b
sachinyadav1777 <sy5892027@gmail.com>
2026-08-31 16:09:07 +0530

    pattern3

---
+++ Patterns/FloydTriangle.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class FloydTriangle {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of rows: ");
+        int rows = sc.nextInt();
+        int count = 1;
+        for(int i = 1; i<=rows; i++) {
+            for(int j = 1; j<=i; j++) {
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
