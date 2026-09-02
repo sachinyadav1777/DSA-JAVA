177fb7cf3454d4ed90365e713282eefa3b25e0d6
sachinyadav1777 <sy5892027@gmail.com>
2026-09-02 19:23:00 +0530

    shortest path between points

---
+++ Strings/Shortest_Path.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class Shortest_Path {
+    public static float getShortestPath(String path) {
+        int x = 0, y = 0;
+        for(int i = 0; i<path.length(); i++) {
+            char dir = path.charAt(i);
+            if(dir == 'N') {
+                y++;
+            } else if (dir == 'S') {
+                y--;
+            } else if (dir == 'W') {
+                x--;
+            }
+            else {
+                x++;
+            }
+        }
+        return (float)Math.sqrt((x * x) + (y * y));
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println("Shortest Path = "+getShortestPath(path));
+    }
+}
