25fcd20fb6da8d3ed8e799f584062937d41f3858
sachinyadav1777 <sy5892027@gmail.com>
2026-09-10 20:15:29 +0530

    buy and sell stocks problem

---
+++ Array_1D/BuyAndSellStocks.java
@@ -0,0 +1,18 @@
+package Array_1D;
+
+public class BuyAndSellStocks {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0; i<prices.length; i++) {
+            if(buyPrice<prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        System.out.println("Profit = "+maxProfit);
+    }
+}
