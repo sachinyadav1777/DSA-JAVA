2057e0a37d20c9b798dadd5d32770f10782ebf31
sachinyadav1777 <sy5892027@gmail.com>
2026-09-11 11:20:17 +0530

    input string is changed

--- Strings/String_Compression.java
+++ Strings/String_Compression.java
@@ -17,7 +17,7 @@ public class String_Compression {
         return sb.toString();
     }
     public static void main(String[] args) {
-        String str = "abc";
+        String str = "aaabbc";
         System.out.println(stringCompressor(str));
     }
 }
