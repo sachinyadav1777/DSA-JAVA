1a1e4e4114ad758b96a001471e4dbfdc25ab0d96
sachinyadav1777 <sy5892027@gmail.com>
2026-09-16 19:42:34 +0530

    class calculator moved inside the parent class

--- OOPS/CompileTime_Polymorphism.java
+++ OOPS/CompileTime_Polymorphism.java
@@ -1,17 +1,17 @@
 package OOPS;
 
-class Calculator {
-    int sum(int a, int b) {
-        return a + b;
-    }
-    float sum(float a, float b) {
-        return a + b;
-    }
-    int sum(int a, int b, int c) {
-        return a + b + c;
-    }
-}
 public class CompileTime_Polymorphism {
+    static class Calculator {
+        int sum(int a, int b) {
+            return a + b;
+        }
+        float sum(float a, float b) {
+            return a + b;
+        }
+        int sum(int a, int b, int c) {
+            return a + b + c;
+        }
+    }
     public static void main(String[] args) {
         Calculator cal = new Calculator();
         System.out.println(cal.sum(7,9));
