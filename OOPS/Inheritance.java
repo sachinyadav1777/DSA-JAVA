7d0f62c4476a247b95545133e44a55a0707b5159
sachinyadav1777 <sy5892027@gmail.com>
2026-09-16 19:50:33 +0530

    all subclass moved inside the parent class

--- OOPS/Inheritance.java
+++ OOPS/Inheritance.java
@@ -1,24 +1,30 @@
 package OOPS;
 
-class Animal {
-    String color;
-
-    void eat() {
-        System.out.println("eats");
-    }
-    void breathe() {
-        System.out.println("breathes");
-    }
-}
-class Fish extends Animal {
-    int fins;
-    void swim() {
-        System.out.println("swim in water");
-    }
-}
 public class Inheritance {
+    static class Animal {
+        String color;
+
+        void eat() {
+            System.out.println("eats");
+        }
+        void breathe() {
+            System.out.println("breathes");
+        }
+    }
+    static class Fish extends Animal {
+        int fins;
+        Fish(int val) {
+            this.fins = val;
+        }
+        void swim() {
+            System.out.println("swim in water");
+        }
+    }
     public static void main(String[] args) {
         Animal a = new Animal();
         a.eat();
+        a.breathe();
+        Fish f = new Fish(2);
+        f.swim();
     }
 }
