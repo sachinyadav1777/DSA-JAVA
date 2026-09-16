8142eb4ed71f078d0d7aea535d025ebee4cd00b7
sachinyadav1777 <sy5892027@gmail.com>
2026-09-16 19:45:28 +0530

    Complex Number concept of mathematics in OOPS

--- OOPS/Complex_Number.java
+++ OOPS/Complex_Number.java
@@ -1,43 +1,43 @@
 package OOPS;
 
-class Complex {
-    int real;
-    int img;
-    Complex(int real, int img) {
-        this.real = real;
-        this.img = img;
-    }
-    static Complex add(Complex c1, Complex c2) {
-        Complex result = new Complex(0,0);
-        result.real = c1.real + c2.real;
-        result.img = c1.img + c2.img;
-        return result;
-    }
-    static Complex sub(Complex c1, Complex c2) {
-        Complex result = new Complex(0,0);
-        result.real = c1.real - c2.real;
-        result.img = c1.img - c2.img;
-        return result;
-    }
-    static Complex product(Complex c1, Complex c2) {
-        Complex result = new Complex(0,0);
-        result.real = (c1.real * c2.real) - (c1.img * c2.img);
-        result.img = (c1.real * c2.img) + (c1.img * c2.real);
-        return result;
-    }
-    public void printResult() {
-        if (real == 0 && img != 0) {
-            System.out.println(img+"i");
-        }
-        else if (real != 0 && img == 0) {
-            System.out.println(real);
-        }
-        else {
-            System.out.println(real+" "+img+"i");
-        }
-    }
-}
 public class Complex_Number {
+    static class Complex {
+        int real;
+        int img;
+        Complex(int real, int img) {
+            this.real = real;
+            this.img = img;
+        }
+        static Complex add(Complex c1, Complex c2) {
+            Complex result = new Complex(0,0);
+            result.real = c1.real + c2.real;
+            result.img = c1.img + c2.img;
+            return result;
+        }
+        static Complex sub(Complex c1, Complex c2) {
+            Complex result = new Complex(0,0);
+            result.real = c1.real - c2.real;
+            result.img = c1.img - c2.img;
+            return result;
+        }
+        static Complex product(Complex c1, Complex c2) {
+            Complex result = new Complex(0,0);
+            result.real = (c1.real * c2.real) - (c1.img * c2.img);
+            result.img = (c1.real * c2.img) + (c1.img * c2.real);
+            return result;
+        }
+        public void printResult() {
+            if (real == 0 && img != 0) {
+                System.out.println(img+"i");
+            }
+            else if (real != 0 && img == 0) {
+                System.out.println(real);
+            }
+            else {
+                System.out.println(real+" "+img+"i");
+            }
+        }
+    }
     public static void main(String[] args) {
         Complex c1 = new Complex(2,3);
         Complex c2 = new Complex(5,4);
