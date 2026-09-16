88cd111a2deb84502451db5f01b7656123225e8d
sachinyadav1777 <sy5892027@gmail.com>
2026-09-16 19:39:41 +0530

    abstraction concept of OOPS

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -1,22 +1,22 @@
 package OOPS;
 
-abstract class AnimalS {
-    void eat() {
-        System.out.println("animal eats");
-    }
-    abstract void walk();
-}
-class Horse extends AnimalS {
-    void walk() {
-        System.out.println("walks on 4 legs");
-    }
-}
-class Chicken extends AnimalS {
-    void walk() {
-        System.out.println("walks on 2 legs");
-    }
-}
 public class Abstraction {
+    static abstract class AnimalS {
+        void eat() {
+            System.out.println("animal eats");
+        }
+        abstract void walk();
+    }
+    static class Horse extends AnimalS {
+        void walk() {
+            System.out.println("walks on 4 legs");
+        }
+    }
+    static class Chicken extends AnimalS {
+        void walk() {
+            System.out.println("walks on 2 legs");
+        }
+    }
     public static void main(String[] args) {
         Horse h = new Horse();
         h.eat();
