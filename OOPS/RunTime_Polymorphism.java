d2f924b5ce6b6fd6a546d1d2ff557b26f62b60ec
sachinyadav1777 <sy5892027@gmail.com>
2026-09-16 19:56:26 +0530

    modified runtime polymorphism

--- OOPS/RunTime_Polymorphism.java
+++ OOPS/RunTime_Polymorphism.java
@@ -1,18 +1,20 @@
 package OOPS;
 
-class Animals {
-    void eat() {
-        System.out.println("Eat anything");
-    }
-}
-class Deer extends Animals {
-    void eat() {
-        System.out.println("Eat Grass");
-    }
-}
 public class RunTime_Polymorphism {
+    static class Animals {
+        void eat() {
+            System.out.println("Eat anything");
+        }
+    }
+    static class Deer extends Animals {
+        void eat() {
+            System.out.println("Eat Grass");
+        }
+    }
     public static void main(String[] args) {
         Animals a = new Animals();
         a.eat();
+        Animals d = new Deer();
+        d.eat();
     }
 }
