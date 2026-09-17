71677fc0ae23de6cb9fb63c4a1fd9e0e360506a2
sachinyadav1777 <sy5892027@gmail.com>
2026-09-17 10:11:35 +0530

    one of the important concept of OOPS

---
+++ OOPS/Encapsulation.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class Encapsulation {
+    static class Student {
+        private String name;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return this.name;
+        }
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Sachin");
+        System.out.println(s1.getName());
+    }
+}
